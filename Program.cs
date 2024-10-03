b319767a641627d0dd93899ee47bc9f25b00fd87
Evilis Glenio <glenio.developer@gmail.com>
2024-10-03 12:37:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Reflection;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("What do you want to do?");
+      Console.WriteLine("0 - Sum");
+      Console.WriteLine("1 - Subtraction");
+      Console.WriteLine("2 - Multiplication");
+      Console.WriteLine("3 - Division");
+      Console.WriteLine("4 - Exit");
+      Console.WriteLine("--------------------");
+      Console.WriteLine("Select an option: ");
+      string? input = Console.ReadLine();
+      if (EOperation.TryParse(input, out EOperation res))
+      {
+        //Switch case
+        switch (res)
+        {
+          case EOperation.Sum:
+            Sum();
+            break;
+          case EOperation.Subtraction:
+            Subtraction();
+            break;
+          case EOperation.Multiplication:
+            Multiplication();
+            break;
+          case EOperation.Division:
+            Division();
+            break;
+          case EOperation.Exit:
+            System.Environment.Exit(0);
+            break;
+          default:
+            Menu();
+            break;
+        }
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter a number.");
+        Menu();
+      }
+    }
+
+    enum EOperation
+    {
+      Sum,
+      Subtraction,
+      Multiplication,
+      Division,
+      Exit
+    }
+
+    static void Sum()
+    {
+      Console.Clear();
+      Console.WriteLine("First value: ");
+      string? input1 = Console.ReadLine();
+      Console.WriteLine("Second value: ");
+      string? input2 = Console.ReadLine();
+      Console.WriteLine("");
+
+      if (float.TryParse(input1, out float v1) && float.TryParse(input2, out float v2))
+      {
+        Console.WriteLine("The result of the sum is: " + (v1 + v2) + "\n");
+        Console.WriteLine("Press any key to return to the menu.");
+        Console.ReadKey();
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter valid numbers.");
+      }
+      Menu();
+    }
+
+    static void Subtraction()
+    {
+      Console.Clear();
+      Console.WriteLine("First value: ");
+      string? input1 = Console.ReadLine();
+      Console.WriteLine("Second value: ");
+      string? input2 = Console.ReadLine();
+      Console.WriteLine("");
+
+      if (float.TryParse(input1, out float v1) && float.TryParse(input2, out float v2))
+      {
+        Console.WriteLine("The result of the subtraction is: " + (v1 - v2));
+        Console.WriteLine("Press any key to return to the menu.");
+        Console.ReadKey();
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter valid numbers.");
+      }
+      Menu();
+    }
+
+    static void Multiplication()
+    {
+      Console.Clear();
+      Console.WriteLine("First value: ");
+      string? input1 = Console.ReadLine();
+      Console.WriteLine("Second value: ");
+      string? input2 = Console.ReadLine();
+      Console.WriteLine("");
+
+      if (float.TryParse(input1, out float v1) && float.TryParse(input2, out float v2))
+      {
+        Console.WriteLine("The result of the multiplication is: " + (v1 * v2));
+        Console.WriteLine("Press any key to return to the menu.");
+        Console.ReadKey();
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter valid numbers.");
+      }
+      Menu();
+    }
+
+    static void Division()
+    {
+      Console.Clear();
+      Console.WriteLine("First value: ");
+      string? input1 = Console.ReadLine();
+      Console.WriteLine("Second value: ");
+      string? input2 = Console.ReadLine();
+      Console.WriteLine("");
+
+      if (float.TryParse(input1, out float v1) && float.TryParse(input2, out float v2))
+      {
+        if (v2 != 0)
+        {
+          Console.WriteLine("The result of the division is: " + (v1 / v2));
+          Console.WriteLine("Press any key to return to the menu.");
+          Console.ReadKey();
+        }
+        else
+        {
+          Console.WriteLine("Cannot divide by zero.");
+          Console.WriteLine("Press any key to return to the menu.");
+          Console.ReadKey();
+        }
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter valid numbers.");
+      }
+      Menu();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,68 @@
+# Calculadora Console em C#
+
+## Descrição
+
+Este é um pequeno projeto de uma calculadora de linha de comando desenvolvida em C#. A aplicação permite realizar operações básicas como soma, subtração, multiplicação e divisão através de um menu interativo. É uma excelente maneira de praticar conceitos fundamentais de programação em C#, como estruturas de controle, tratamento de entradas do usuário e manipulação de dados.
+
+## Funcionalidades
+
+- **Soma**: Adicione dois números.
+- **Subtração**: Subtraia um número de outro.
+- **Multiplicação**: Multiplique dois números.
+- **Divisão**: Divida um número por outro, com verificação para divisão por zero.
+- **Sair**: Encerra a aplicação.
+
+## Pré-requisitos
+
+- **.NET SDK**: Certifique-se de ter o [.NET SDK](https://dotnet.microsoft.com/download) instalado na sua máquina. Esta aplicação foi desenvolvida com base no .NET 6.0, mas versões mais recentes também devem funcionar.
+
+## Como Executar
+
+1. **Clonar o Repositório**
+
+   ```bash
+   git clone https://github.com/evilisglenio/calculadora-console-csharp.git
+   ```
+
+2. **Navegar até o Diretório**
+   ```bash
+   cd calculadora-console-csharp
+   ```
+3. **Compilar e Executar**
+   ```bash
+   dotnet run
+   ```
+
+## Como Usar
+
+A aplicação exibirá um menu interativo com as seguintes opções:
+
+```bash
+What do you want to do?
+0 - Sum
+1 - Subtraction
+2 - Multiplication
+3 - Division
+4 - Exit
+--------------------
+Select an option:
+```
+
+1. **Selecione uma Operação**: Escolha uma das operações disponíveis no menu principal.
+2. **Insira os Números**: Digite os números necessários para a operação selecionada.
+3. **Obtenha o Resultado**: A aplicação exibirá o resultado da operação.
+4. **Encerrar**: Para sair da aplicação, escolha a opção "Sair" no menu principal.
+
+## Contribuição
+
+Se você deseja contribuir para este projeto, sinta-se à vontade para abrir um pull request ou relatar problemas.
+
+## Licença
+
+## Este projeto está licenciado sob a [MIT License](LICENSE).
+
+---
+
+## Autor
+
+- [Evilisglenio](https://github.com/evilisglenio)
